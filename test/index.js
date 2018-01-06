348b06770a350f62d3ba8d4c2ff7eddbaafedbd0
Jordan Harband <ljharb@gmail.com>
2018-01-06 00:11:52 -0800

    Tests.

---
+++ test/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var test = require('tape');
+
+var hasTemplateLiterals = require('../');
+
+test('export', function (t) {
+	t.equal(typeof hasTemplateLiterals, 'function', 'export is a function');
+	t.end();
+});
+
+test('template literal support', function (t) {
+	var yes = hasTemplateLiterals();
+	t.equal(typeof yes, 'boolean', 'export returns a boolean');
+
+	var makeTemplateLiteral = function () { Function('`a template literal`'); };
+
+	t.test('no template literal support', { skip: yes }, function (st) {
+		st.equal(yes, false, 'returns false');
+		st['throws'](makeTemplateLiteral, 'throws on template literal syntax');
+		st.end();
+	});
+
+	t.test('has template literal support', { skip: !yes }, function (st) {
+		st.equal(yes, true, 'returns true');
+		st.doesNotThrow(makeTemplateLiteral, 'does not throw on template literal syntax');
+		st.end();
+	});
+});
